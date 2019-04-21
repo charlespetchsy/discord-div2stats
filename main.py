be7cc5437d53feec5b49c6b0f797196734f4b1a4
charlespetchsy <petchsycp@gmail.com>
2019-04-21 18:54:02 -0400

    add initial parser class

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+__author__ = "Charles Petchsy"
+
+__license__ = "MIT"
+__maintainer__ = "Charles Petchsy"
+__status__ = "Alpha"
+
+import requests
+import json
+import ast
 import discord
+
 from discord.ext import commands
+from html.parser import HTMLParser
+
+class Lifetime(HTMLParser):
+    """ Parse requirements:
+    Each entry will include the rank percentile of the player.
+    PvP Kills
+    NPC Kills
+    Skill Kills
+    Headshots
+    Items Looted
+    E-Credit Balance
+    Commendations
+    Curr Comm. Score
+    Sharpshooter Kills
+    Survivalist Kills
+    Demolitionist Kills
+    """
+    def handle_data(self, data):
+        if 'window.__INITIAL_STATE__' in data:
+            player_data = json.loads(
+                ast.literal_eval(
+                    repr(data.split(';')[0].split('=')[1])
+                    )
+                )
+            # Get player data
+            print(player_data['stats'])
 
 client = discord.Client()
 @client.event
@@ -15,12 +51,20 @@ async def on_message(message):
     if message.content.lower() == "!test":
         await message.channel.send("I'm alive.")
 
-# bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='!')
 
-# @bot.command()
-# async def test(ctx, arg):
-#     await ctx.channel.send(arg)
+@bot.command()
+async def div2(ctx, arg):
+    await ctx.channel.send(arg)
 
-# # Add commands and run with token here
-# bot.run('')
-client.run('')
+if __name__ == '__main__':
+    # Add commands and run with token here
+    # bot.run('')
+    # client.run('')
+
+    r = requests.get('https://division.tracker.gg/division-2/profile/uplay/Softbunn/overview')
+    # print(r.content)
+
+    # instantiate the parser and fed it some HTML
+    parser = Lifetime()
+    parser.feed(str(r.content))
